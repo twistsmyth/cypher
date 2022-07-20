5d6da3a005f411114dae581e3542e1c5ed0d2f59
redbloc <red@bloc.bit>
2022-07-20 12:16:42 +0200

    corrected for wallet

--- install/netDietPi.sh
+++ install/netDietPi.sh
@@ -1,7 +1,14 @@
-su dietpi
-sudo apt-get install libicu-dev -y
+# dietpi install
+# su dietpi
+# bash <(curl -sSL https://raw.githubusercontent.com/twistsmyth/cypher/docker/install/netDietPi.sh)
+# bash <(curl -sSL https://raw.githubusercontent.com/cypher-network/cypher/master/install/install.sh)
+# bash <(curl -sSL https://raw.githubusercontent.com/cypher-network/bamboo/master/install/install.sh)
+# sudo systemctl status cypher-cypnode
+# clibamwallet
+sudo apt-get install libicu-dev libgmp-dev libsodium-dev libssl-dev libatomic1 -y
 cd /tmp
 curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel Current
 echo 'export DOTNET_ROOT=$HOME/.dotnet' >> ~/.bashrc
 echo 'export PATH=$PATH:$HOME/.dotnet' >> ~/.bashrc
 source ~/.bashrc
+dotnet --version
