f7d59417c1d8aa4039fd001c675ab15de38a8e1c
redbloc <red@bloc.bit>
2022-07-20 08:47:27 +0200

    test

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM archlinux AS build-env
-WORKDIR /app
-
-RUN pacman -Sy dotnet-runtime
-bash <(curl -sSL https://raw.githubusercontent.com/twistsmyth/cypher/master/install/installDocker.sh)
-
---
+++ install/netDietPi.sh
@@ -0,0 +1,7 @@
+su dietpi
+sudo apt-get install libicu-dev -y
+cd /tmp
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel Current
+echo 'export DOTNET_ROOT=$HOME/.dotnet' >> ~/.bashrc
+echo 'export PATH=$PATH:$HOME/.dotnet' >> ~/.bashrc
+source ~/.bashrc
